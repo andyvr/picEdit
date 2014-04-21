013fc3d23fe5c911f863228c20964b5022999e10
Andrei V <avarabyou@gmail.com>
2014-04-21 13:54:58 -0400

    Update builder file/reorganize folder structure

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 picEdit
 =======
 
-*This plugin is work in progress!* The plugin will turn your form's file-input box into a tiny image editor. Perform basic image operations and preview your image before uploading to the server. The functionality for the plugin is very similar to the one found in http://pasteboard.co
+*This plugin is work in progress!* The plugin will turn your form's file-input box into a tiny image editor. Perform basic image operations and preview your image before uploading to the server. The functionality for the plugin is very similar to the one found in pasteboard.co
 
 So, your good old html form with the file upload field to upload an image:
 <p align="center">
@@ -12,12 +12,13 @@ will change to something more user-friendly:
 <img src="../../raw/gh-pages/img/img2.gif" />
 </p>
 
-The plugin supports 2 sources for image data:<br>
-1. You can drag-and-drop or search your computer for images<br>
-2. You can use your computer's web-camera to make a photo
+The plugin supports 3 sources for image data:<br>
+1. You can drag-and-drop your image on the widget or search your computer for images<br>
+2. You can use your computer/tablet web-camera to make a photo<br>
+3. You can copy and paste the image
 
 The plugin supports image rotation, cropping, resizing and pen tool.
-Once you're done with the image manipulation you can just upload your form as usual, the updated image will be uploaded along with the form, as if it would be the regular file-upload field.
+Once you're done with the image manipulations you can just upload your form as usual, the updated image will be uploaded along with the form, as if it would be the regular file-upload field.
 
 See the demo here: http://andyvr.github.io/picEdit/
 
--- build/build.py
+++ build/build.py
@@ -6,46 +6,89 @@ import re
 from httplib2 import Http
 from urllib import urlencode
 
-# cd to the script directory
-try: __file__
-except NameError:
-    basepath = '/Users/andreivarabyou/Documents/git/picEdit/build/'
-else:
-    basepath = os.path.dirname(__file__)
-if basepath != "":
-    os.chdir(basepath)
+def main():
+    # cd to the script directory
+    try: __file__
+    except NameError:
+        basepath = '/Users/andreivarabyou/Documents/git/picEdit/build/'
+    else:
+        basepath = os.path.dirname(__file__)
+    if basepath != "":
+        os.chdir(basepath)
+    compile_html()
+    compile_css()
+    compile_js()
+    
+def compile_js():
+    file = open("../dist/js/picedit.js", "r")
+    src_js = file.read()
+    file.close()
+    # get the plugin description
+    title = re.compile(r'^\/\*.+?\*\/', re.DOTALL | re.IGNORECASE)
+    title = re.findall(title, src_js)
+    # minify the js
+    url = 'http://javascript-minifier.com/raw'   
+    body = {'input': src_js}
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
+    http = Http()
+    resp, content = http.request(url, "POST", headers=headers, body=urlencode(body))
+    file = open("../dist/js/picedit.min.js", "w")
+    file.write(title[0] + "\n" + content)
+    file.close()
 
-# loads sources from the disk
-file = open("../src/index.html", "r")
-src_html = file.read()
-file.close()
-file = open("../src/js/picedit.js", "r")
-src_js = file.read()
-file.close()
+def compile_css():
+    # concatenate styles and save destination unminified
+    file = open("../src/css/font.css", "r")
+    src_style1 = file.read()
+    file.close()
+    file = open("../src/css/styles.css", "r")
+    src_style2 = file.read()
+    file.close()
+    file = open("../dist/css/styles.css", "w")
+    css_source = src_style1 + "\n" + src_style2
+    file.write(css_source)
+    file.close()
+    # minify the css
+    url = 'http://cssminifier.com/raw'   
+    body = {'input': css_source}
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
+    http = Http()
+    resp, content = http.request(url, "POST", headers=headers, body=urlencode(body))
+    file = open("../dist/css/styles.min.css", "w")
+    file.write(content)
+    file.close()
 
-# extract html code
-phtm = re.compile(r'<!-- begin_picedit_box -->.+<!-- end_picedit_box -->', re.IGNORECASE | re.DOTALL)
-pouthtm = re.findall(phtm, src_html)
-
-# minify the html
-url = 'http://www.willpeavy.com/minifier/'   
-body = {'html': pouthtm[0]}
-headers = {'Content-type': 'application/x-www-form-urlencoded'}
-http = Http()
-resp, content = http.request(url, "POST", headers=headers, body=urlencode(body))
-outhtml = re.findall('(<textarea.+?>)(.+?)(</textarea)', content)
-
-#comment and uncomment source and dist code
-unc = re.compile('(\/\*unhide_in_prod\*\/.*?)(\/\*)(.+?)(\*\/)(.*\/\*unhide_in_prod\*\/)', re.IGNORECASE | re.DOTALL)
-src_js = unc.sub(r'\1 \3 \5', src_js)
-unc = re.compile('(\/\*hide_in_prod\*\/)(.+)(\/\*hide_in_prod\*\/)', re.IGNORECASE | re.DOTALL)
-src_js = unc.sub(r'\1 /* \2 */ \3', src_js)
-
-#apply compiled html
-unc = re.compile('compiled_template_markup', re.DOTALL)
-src_js = unc.sub(outhtml[0][1], src_js)
-
-#save pre-processed js to the dist folder
-file = open("../dist/js/picedit.js", "w")
-file.write(src_js)
-file.close()
+def compile_html():
+    # loads sources from the disk
+    file = open("../src/index.html", "r")
+    src_html = file.read()
+    file.close()
+    file = open("../src/js/picedit.js", "r")
+    src_js = file.read()
+    file.close()
+    # extract html code
+    phtm = re.compile(r'<!-- begin_picedit_box -->.+<!-- end_picedit_box -->', re.IGNORECASE | re.DOTALL)
+    pouthtm = re.findall(phtm, src_html)
+    # minify the html
+    url = 'http://www.willpeavy.com/minifier/'   
+    body = {'html': pouthtm[0]}
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
+    http = Http()
+    resp, content = http.request(url, "POST", headers=headers, body=urlencode(body))
+    outhtml = re.findall('(<textarea.+?>)(.+?)(</textarea)', content)
+    #comment and uncomment source and dist code
+    unc = re.compile('(\/\*unhide_in_prod\*\/.*?)(\/\*)(.+?)(\*\/)(.*\/\*unhide_in_prod\*\/)', re.IGNORECASE | re.DOTALL)
+    src_js = unc.sub(r'\1 \3 \5', src_js)
+    unc = re.compile('(\/\*hide_in_prod\*\/)(.+)(\/\*hide_in_prod\*\/)', re.IGNORECASE | re.DOTALL)
+    src_js = unc.sub(r'\1 /* \2 */ \3', src_js)
+    #apply compiled html
+    unc = re.compile('compiled_template_markup', re.DOTALL)
+    src_js = unc.sub(outhtml[0][1], src_js)
+    #save pre-processed js to the dist folder
+    file = open("../dist/js/picedit.js", "w")
+    file.write(src_js)
+    file.close()
+    
+# run the main function
+if __name__ == "__main__":
+    main()
